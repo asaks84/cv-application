680e4fc236d17f6899272dcfeef1d4563f49de49
Cauê Carrera (W) <cauecpl@gmail.com>
2025-02-07 00:58:49 -0300

    Print version adjust

--- src/Components/Infos/JobInfo.jsx
+++ src/Components/Infos/JobInfo.jsx
@@ -6,7 +6,7 @@ function JobInfo(){
     <div className="position-relative mt-1">
         <p className="date mb-0">Feb/2025 until now</p>
         <h4 className='fw-bold mb-0'><span className='position'>Position /</span> Company Name</h4>
-        <p className="desciption mb-2 text-black">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nobis quasi similique dicta ducimus provident modi doloremque! Eaque voluptatibus at beatae eveniet sequi deserunt in vel, facere ea non quibusdam?</p>
+        <p className="desciption text-black">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nobis quasi similique dicta ducimus provident modi doloremque! Eaque voluptatibus at beatae eveniet sequi deserunt in vel, facere ea non quibusdam?</p>
         <EditBtn specClass={'start-n1'}/>
       </div>
   )
