5050e03d327553b8999516b92b1a5a0c0687b9f7
Cauê Carrera (W) <cauecpl@gmail.com>
2025-02-08 14:00:05 -0300

    Name Component editable done

--- src/Components/Infos/BasicInfo.jsx
+++ src/Components/Infos/BasicInfo.jsx
@@ -13,150 +13,156 @@ function BasicInfo() {
     }
   })
 
-  const handleInfo = (e) => {
-
-    const { name, value } = e.target;
+  const handleInfo = (e) => { 
+    const { name, value } = e.target; // getting attribute name and value
 
     setInfo((prev) => {
-      if (name.includes('portfolio')) {
-        return {
-          ...prev,
+      if (name.includes('portfolio')) { // check if it's a portfolio info
+        return {  // return an object
+          ...prev, // make a copy of the original object to edit
           portfolio: {
-            ...prev.portfolio,
-            [name.split(".")[1]]: value,
+            ...prev.portfolio, // set the portfolio key to edit
+            [name.split(".")[1]]: value.trim(), // use word after '.' as a key for new object
           },
         };
       }
-
+      // not being a portfolio name attribute return an object
       return {
-        ...prev,
-        [name]: value,
+        ...prev, // amking a copy from original to edit 
+        [name]: value, // setting name as a key from object
       }
-  })
+    })
 
-}
+  }
 
-return (
-  <>
-    <div className="col basicInfo d-flex flex-row justify-content-end align-items-stretch position-relative p-0">
-      <ul className='list-unstyled d-flex flex-column gap-2 text-end pt-3'>
-        <li>
-          {info.address}
-          <i className='bi bi-house-fill'> </i>
-        </li>
-        <li>
-          {info.phone}
-          <i className='bi bi-phone-fill'> </i>
-        </li>
-        <li>
-          {info.email}
-          <i className='bi bi-envelope-fill'> </i>
-        </li>
-        <li>
-          <a href={info.portfolio.link}>{info.portfolio.text}</a>
-          <i className='bi bi-globe'></i>
-        </li>
-      </ul>
-      <EditBtn modalName={'#basicInfoModal'} />
+  return (
+    <>
+      <div className="col basicInfo d-flex flex-row justify-content-end align-items-stretch position-relative p-0">
+        <ul className='list-unstyled d-flex flex-column gap-2 text-end pt-3'>
+          <li>
+            {info.address}
+            <i className='bi bi-house-fill'> </i>
+          </li>
+          <li>
+            {info.phone}
+            <i className='bi bi-phone-fill'> </i>
+          </li>
+          <li>
+            {info.email}
+            <i className='bi bi-envelope-fill'> </i>
+          </li>
+          <li>
+            {(info.portfolio.text !== '')
+              ? <>
+                <a href={info.portfolio.link} target='_blank'>{info.portfolio.text}</a>
+                <i className='bi bi-globe'></i>
+              </>
+              : <li>&nbsp;</li>
+            }
+          </li>
 
-      {/* Modal Bootstrap */}
-      <div className="modal fade" id="basicInfoModal" tabIndex="-1" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Edit Item</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
+        </ul>
+        <EditBtn modalName={'#basicInfoModal'} />
 
-              <div className="input-group mb-3">
-                <span className="input-group-text bi bi-house-fill"></span>
-                <input
-                  name='address'
-                  type="text"
-                  className="form-control"
-                  aria-label="Address"
-                  aria-describedby="basic-addon1"
-                  value={info.address}
-                  onChange={handleInfo}
-                  placeholder="Your Address"
-                />
+        {/* Modal Bootstrap */}
+        <div className="modal fade" id="basicInfoModal" tabIndex="-1" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Edit Item</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
+              <div className="modal-body">
 
-              <div className="input-group mb-3">
-                <span className="input-group-text bi bi-phone-fill"></span>
-                <input
-                  name='phone'
-                  type="text"
-                  className="form-control"
-                  aria-label="Address"
-                  aria-describedby="basic-addon1"
-                  value={info.phone}
-                  onChange={handleInfo}
-                  placeholder="Your Address"
-                />
+                <div className="input-group mb-3">
+                  <span className="input-group-text bi bi-house-fill"></span>
+                  <input
+                    name='address'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={info.address}
+                    onChange={handleInfo}
+                    placeholder="Your Address"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <span className="input-group-text bi bi-phone-fill"></span>
+                  <input
+                    name='phone'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={info.phone}
+                    onChange={handleInfo}
+                    placeholder="Your Address"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <span className="input-group-text bi bi-envelope-fill"></span>
+                  <input
+                    name='email'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={info.email}
+                    onChange={handleInfo}
+                    placeholder="Your Address"
+                  />
+                </div>
+
+                <label htmlFor="portfolio">Portfolio info</label>
+                <div className="input-group mb-3">
+                  <span className="input-group-text bi bi-globe"></span>
+                  <input
+                    name='portfolio.text'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={info.portfolio.text}
+                    onChange={handleInfo}
+                    placeholder="Text of your portfolio"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <span className="input-group-text">Link</span>
+                  <input
+                    name='portfolio.link'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={info.portfolio.link}
+                    onChange={handleInfo}
+                    placeholder="(00) 00000.0000"
+                  />
+                </div>
+
               </div>
-
-              <div className="input-group mb-3">
-                <span className="input-group-text bi bi-envelope-fill"></span>
-                <input
-                  name='email'
-                  type="text"
-                  className="form-control"
-                  aria-label="Address"
-                  aria-describedby="basic-addon1"
-                  value={info.email}
-                  onChange={handleInfo}
-                  placeholder="Your Address"
-                />
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  id="closeModalBtn"
+                >Close</button>
               </div>
-
-              <div className="input-group mb-3">
-                <span className="input-group-text bi bi-globe"></span>
-                <input
-                  name='portfolio.text'
-                  type="text"
-                  className="form-control"
-                  aria-label="Address"
-                  aria-describedby="basic-addon1"
-                  value={info.portfolio.text}
-                  onChange={handleInfo}
-                  placeholder="Homepage"
-                />
-              </div>
-
-              <div className="input-group mb-3">
-                <span className="input-group-text">Link</span>
-                <input
-                  name='portfolio.link'
-                  type="text"
-                  className="form-control"
-                  aria-label="Address"
-                  aria-describedby="basic-addon1"
-                  value={info.portfolio.link}
-                  onChange={handleInfo}
-                  placeholder="(00) 00000.0000"
-                />
-              </div>
-
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-                id="closeModalBtn"
-              >Close</button>
             </div>
           </div>
         </div>
+
       </div>
 
-    </div>
 
-
-  </>
-)
+    </>
+  )
 }
 
 export default BasicInfo
--- src/Components/Infos/Name.jsx
+++ src/Components/Infos/Name.jsx
@@ -1,13 +1,77 @@
+import { useState } from 'react'
 import EditBtn from '../Elements/EditBtn'
 
 function Name() {
 
+  const [myName, setName] = useState({first: 'Name', last: 'Last Name'});
+
+  const handleName = (e) => {
+    const {name, value} = e.target;
+    console.log(name)
+
+    setName( {...myName, [name]:value } );
+    
+  }
+
   return (
-    <div className="col-auto text-start fs-1 position-relative z-1">
-      <h1 className="first m-0 display-5 fw-bold">Name</h1>
-      <h1 className="last m-0 display-5 fw-bold text-nowrap">Last Name</h1>
-      <EditBtn />
+    <>
+      <div className="col-auto text-start fs-1 position-relative z-1">
+      <h1 className="first m-0 display-5 fw-bold">{myName.first}</h1>
+      <h1 className="last m-0 display-5 fw-bold text-nowrap">{myName.last}</h1>
+      <EditBtn modalName={'#nameModal'}/>
     </div>
+
+      {/* Modal Bootstrap */}
+      <div className="modal fade" id="nameModal" tabIndex="-1" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Edit Item</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+
+                <div className="input-group mb-3">
+                  <span className="input-group-text">First Name</span>
+                  <input
+                    name='first'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={myName.first}
+                    onChange={handleName}
+                    placeholder="Write Your Name"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <span className="input-group-text">Last Name</span>
+                  <input
+                    name='last'
+                    type="text"
+                    className="form-control"
+                    aria-label="Address"
+                    aria-describedby="basic-addon1"
+                    value={myName.last}
+                    onChange={handleName}
+                    placeholder="Your Last name"
+                  />
+                </div>
+
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  id="closeModalBtn"
+                >Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
